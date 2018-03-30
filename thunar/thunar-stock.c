9a84ec80b8aed9ce36a09de9f930a077511c0a39
Andre Miranda <andreldm@xfce.org>
2018-03-30 20:34:08 -0300

    Use symbolic icons for file transfer dialog
    Drop custom shortcuts icon in favor of "user-bookmarks"

--- thunar/thunar-stock.c
+++ thunar/thunar-stock.c
@@ -46,7 +46,7 @@ typedef struct
 static const ThunarStockIcon thunar_stock_icons[] =
 {
   { THUNAR_STOCK_DESKTOP,     "user-desktop" },
-  { THUNAR_STOCK_SHORTCUTS,   "stock_thunar-shortcuts" },
+  { THUNAR_STOCK_SHORTCUTS,   "user-bookmarks" },
   { THUNAR_STOCK_TEMPLATES,   "text-x-generic-template" },
   { THUNAR_STOCK_TRASH_EMPTY, "user-trash",   },
   { THUNAR_STOCK_TRASH_FULL,  "user-trash-full" },
